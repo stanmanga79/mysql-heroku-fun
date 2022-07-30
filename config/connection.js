da39a622f7170f3171ad311f0879e9658a30bffc
Stanely Lewis <stanmanga79@gmail.com>
2022-07-30 14:04:10 -0400

    Update connection feature

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.MYSQLURI)
+const sequelize = new Sequelize(process.env.JAWSDB_URL || process.env.MYSQLURI)
 
 module.exports = sequelize;
